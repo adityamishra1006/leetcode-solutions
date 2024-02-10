8e917fc8f29bfca84c6e728f1aba2f58997a48f0
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-02-11 01:56:34 +0530

    Create 118_pascalTriangleOptimized.java

---
+++ 118_pascalTriangleOptimized.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0; i<n; i++){
+            List<Integer> l = new ArrayList<>();
+            for(int j = 0; j <= i; j++){
+                if(j==0 || j==i) l.add(1);
+                else{
+                    l.add(ans.get(i-1).get(j) + ans.get(i-1).get(j-1));
+                }
+            }
+            ans.add(l);
+        }
+        return ans;
+    }
+}
