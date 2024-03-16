aba3666c7ea7026832c7a408475c2ceb1a96310b
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-03-17 01:44:18 +0530

    Create 2095_DeleteTheMiddleOfALinkedList.java

---
+++ 2095_DeleteTheMiddleOfALinkedList.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null){
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast= head;
+        ListNode curr= head;
+
+        while(fast!=null && fast.next!=null){
+            curr = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        curr.next = slow.next;
+        return head;
+
+    }
+}
