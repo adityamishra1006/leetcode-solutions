51ba8c973a1eb969a6591b25aadaa7f21d2b91e7
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2024-04-05 16:22:19 +0530

    Create 2130_MaximumTwinSumOfALinkedList.java

---
+++ 2130_MaximumTwinSumOfALinkedList.java
@@ -0,0 +1,47 @@
+class Solution {
+    public ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode Next = null;
+
+        while(curr!=null){
+            Next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = Next;
+        }
+        return prev;
+    }
+    public int pairSum(ListNode head) {
+        if(head==null) return 0;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        ListNode temp = null;
+
+        while(fast!=null && fast.next!=null){ 
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode reversed = reverse(slow);
+        if(prev!=null){
+            prev.next=reversed;
+        }
+
+        int ans = 0;
+        ListNode i = head;
+        ListNode j = reversed;
+
+        while(i!=null && j!=null){
+            ans = Math.max(ans, i.val+j.val);
+            i = i.next;
+            j = j.next;
+        }
+        if(prev!=null){
+            prev.next=reverse(reversed);
+        }
+        return ans;
+    }
+}
